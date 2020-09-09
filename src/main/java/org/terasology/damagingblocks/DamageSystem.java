852ccade8527c0e1d42b3a5fff10428092df8669
DarkWeird <foxflameless@gmail.com>
2020-09-09 17:06:44 +0300

    feature(reintegrate-gestaltv7):  gestalt-v7 compability (package)

--- src/main/java/org/terasology/damagingblocks/DamageSystem.java
+++ src/main/java/org/terasology/damagingblocks/DamageSystem.java
@@ -1,41 +1,28 @@
-/*
- * Copyright 2016 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2020 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.damagingblocks;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.terasology.damagingblocks.component.DamagingBlockComponent;
 import org.terasology.damagingblocks.component.DamagedByBlockComponent;
-import org.terasology.engine.Time;
-import org.terasology.entitySystem.entity.EntityManager;
-import org.terasology.entitySystem.entity.EntityRef;
-import org.terasology.entitySystem.event.ReceiveEvent;
-import org.terasology.entitySystem.systems.BaseComponentSystem;
-import org.terasology.entitySystem.systems.RegisterMode;
-import org.terasology.entitySystem.systems.RegisterSystem;
-import org.terasology.entitySystem.systems.UpdateSubscriberSystem;
-import org.terasology.logic.characters.events.OnEnterBlockEvent;
-import org.terasology.logic.health.event.DoDamageEvent;
-import org.terasology.logic.health.EngineDamageTypes;
-import org.terasology.logic.inventory.PickupComponent;
-import org.terasology.logic.location.LocationComponent;
+import org.terasology.damagingblocks.component.DamagingBlockComponent;
+import org.terasology.engine.core.Time;
+import org.terasology.engine.entitySystem.entity.EntityManager;
+import org.terasology.engine.entitySystem.entity.EntityRef;
+import org.terasology.engine.entitySystem.event.ReceiveEvent;
+import org.terasology.engine.entitySystem.systems.BaseComponentSystem;
+import org.terasology.engine.entitySystem.systems.RegisterMode;
+import org.terasology.engine.entitySystem.systems.RegisterSystem;
+import org.terasology.engine.entitySystem.systems.UpdateSubscriberSystem;
+import org.terasology.engine.logic.characters.events.OnEnterBlockEvent;
+import org.terasology.engine.logic.destruction.EngineDamageTypes;
+import org.terasology.engine.logic.inventory.PickupComponent;
+import org.terasology.engine.logic.location.LocationComponent;
+import org.terasology.engine.registry.In;
+import org.terasology.engine.world.BlockEntityRegistry;
+import org.terasology.engine.world.WorldProvider;
+import org.terasology.health.logic.event.DoDamageEvent;
 import org.terasology.math.geom.Vector3f;
-import org.terasology.registry.In;
-import org.terasology.world.BlockEntityRegistry;
-import org.terasology.world.WorldProvider;
 
 @RegisterSystem(RegisterMode.AUTHORITY)
 public class DamageSystem extends BaseComponentSystem implements UpdateSubscriberSystem {
@@ -54,15 +41,15 @@ public class DamageSystem extends BaseComponentSystem implements UpdateSubscribe
     private WorldProvider worldProvider;
 
     /**
-     * Calculates when the player should be given damage and applies damage to the players.
-     * Also destroys pickable items if inside DamagingBlocks
+     * Calculates when the player should be given damage and applies damage to the players. Also destroys pickable items
+     * if inside DamagingBlocks
      *
      * @param delta The time between frames (optional to account for lagging games)
      */
     @Override
     public void update(float delta) {
         long gameTime = time.getGameTimeInMs();
-        
+
         for (EntityRef entity : entityManager.getEntitiesWith(DamagedByBlockComponent.class, LocationComponent.class)) {
             DamagedByBlockComponent damaged = entity.getComponent(DamagedByBlockComponent.class);
             LocationComponent loc = entity.getComponent(LocationComponent.class);
@@ -71,7 +58,7 @@ public class DamageSystem extends BaseComponentSystem implements UpdateSubscribe
                 //damage the entity
                 EntityRef block = blockEntityProvider.getBlockEntityAt(loc.getWorldPosition());
                 DamagingBlockComponent damaging = block.getComponent(DamagingBlockComponent.class);
-                
+
                 if (damaging != null) {
                     entity.send(new DoDamageEvent(damaging.blockDamage, EngineDamageTypes.PHYSICAL.get(), block));
                     // set the next damage time
@@ -100,9 +87,8 @@ public class DamageSystem extends BaseComponentSystem implements UpdateSubscribe
     }
 
     /**
-     * Marks the player as possibly about to be damaged if they enter a new block.
-     * As the player entity hasn't actually been moved there yet and the event
-     * doesn't include the coordinates, checking that the block is actually damaging
+     * Marks the player as possibly about to be damaged if they enter a new block. As the player entity hasn't actually
+     * been moved there yet and the event doesn't include the coordinates, checking that the block is actually damaging
      * is deferred to later.
      */
     @ReceiveEvent
